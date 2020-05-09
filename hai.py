1142bbb4c2367c4198ff492fa4ba5b0a1871ad59
samir <samir@sgmbp>
2020-05-09 13:42:58 +1000

    Refactored and made it better

--- hai.py
+++ hai.py
@@ -4,23 +4,48 @@ import sys
 import re
 import binascii
 
-
 test_hex_1 = "0x41 0x42 0x43"
+test_hex_4 = "x41 x42 x43"
 test_hex_2 = "0x414243"
 test_hex_3 = "\\x41\\x42\\x43"
-test_hex_4 = "x41 x42 x43"
 test_hex_5 = "414243"
 
-#FORMAT_DEFAULT = '\x5Cx{}'
 FORMAT_DEFAULT = '\\x{}'
-#FORMAT_DEFAULT = '0x{} '
+# Other format -> FORMAT_DEFAULT = '0x{} '
 
 rx_numbers = r'^[0-9]+$'
 rx_hex = r'^(0x|x|\\x)?([0-9a-fA-F]{2,}$)'
 rx_hex_pairs = r'^(\\x[0-9a-fA-F]{2}\s?)+$'
 
 
+def print_hai(h, a, i):
+    """ Prints out HAI data """
+    print("Hex:     {}".format(h))
+    print("Ascii:   {}".format(a))
+    print("Int:     {}".format(i))
 
+
+def hex_to_pretty(hex_str, hex_format=FORMAT_DEFAULT):
+    """
+    Take a hex string like '31324a4b' and turns it into a formatted version
+    like '\\x31\\x32\\x4a\\x4b' or what ever FORMAT is specified
+    """
+    out_hex = ""
+    out_int = []
+    out_ascii = ""
+
+    unhex = binascii.unhexlify(hex_str)
+    out_ascii = unhex
+    if len(unhex) == 1:
+        v_int = ord(unhex)
+        out_hex = hex_format.format(hex(v_int)[2:])
+        out_int = [v_int]
+    else:
+        for i in unhex:
+            v_int = ord(i)
+            out_int.append(v_int)
+            out_hex = out_hex + hex_format.format(hex(v_int)[2:])
+    return (out_hex, out_ascii, out_int)
 
 
 def characterize_data(data_string):
@@ -28,143 +53,59 @@ def characterize_data(data_string):
     Input will be a string from command-line need to figure out the format (best guess)
     """
     out_int = []
-    #out_hex = []
     out_hex = ""
     out_ascii = []
 
     # Split on spaces
     data_split = data_string.split()
     if len(data_split) == 1:
-        # Only on continous string, no spaces...
-        print(data_split[0])
 
-        # Check if only integers
-        mtch = re.match(rx_numbers, data_split[0])
+        # Only integers?
+        mtch = re.match(rx_numbers, data_string)
         if mtch:
-            # This is most likely an integer
+            print("Input: {} - INT".format(mtch.group(0)))
             data_int = int(data_string)
-            print("Integer?")
-            print(data_int)
             hex_str = hex(data_int)[2:]
             if len(hex_str) % 2:
                 hex_str = "0" + hex_str
-            #print(hex(data_int))
-            unhex = binascii.unhexlify(hex_str)
-            for i in unhex:
-                value_int = ord(i)
-                out_int.append(value_int)
-                out_hex = out_hex + FORMAT_DEFAULT.format(hex(value_int)[2:])
-            print(out_hex)
-            #print(hex_str)
-            print(unhex)
+            (h, a, i) = hex_to_pretty(hex_str)
+            print_hai(h, a, i)
             return
 
-        # Single hex value
-        mtch = re.match(rx_hex, data_split[0])
+        # Single hex value?
+        mtch = re.match(rx_hex, data_string)
         if mtch:
-            print("Hex?")
+            print("Input: {} - HEX".format(mtch.group(0)))
             value = mtch.group(2)
-            print(value)
-            unhex = binascii.unhexlify(value)
-            if len(unhex) == 1:
-                value_int = ord(unhex)
-                print(value_int)
-                print(FORMAT_DEFAULT.format(hex(value_int)[2:]))
-                print(unhex)
-            else:
-                for i in unhex:
-                    value_int = ord(i)
-                    out_int.append(value_int)
-                    out_hex = out_hex + FORMAT_DEFAULT.format(hex(value_int)[2:])
-                print(out_int)
-                print(out_hex)
-                print(unhex)
+            (h, a, i) = hex_to_pretty(value)
+            print_hai(h,a,i)
             return
 
-        # Repeated hex
-        mtch = re.match(rx_hex_pairs, data_split[0])
+        # Repeated hex value?
+        mtch = re.match(rx_hex_pairs, data_string)
         if mtch:
+            print("Input: {} - HEX-REPEATED".format(mtch.group(0)))
             v_split = mtch.group(0).split('\\x')
             value = ''.join(v_split)
-            unhex = binascii.unhexlify(value)
-            for i in unhex:
-                value_int = ord(i)
-                out_int.append(value_int)
-                #out_hex.append('\x5C' + hex(value_int)[1:])
-                out_hex = out_hex + '\x5C' + hex(value_int)[1:]
-            print(out_int)
-            print(out_hex)
-            print(unhex)
+            (h, a, i) = hex_to_pretty(value)
+            print_hai(h, a, i)
             return
 
+        # Something else, maybe byte array?
         else:
-            print("Byte array?")
-            ba = bytearray(data_split[0])
-            for i in ba:
-                out_int.append(i)
-                #out_hex.append(hex(i))
-                out_hex = out_hex + '\x5C' + hex(i)[1:]
-                out_ascii.append(chr(i))
-            print(out_int)
-            print(out_hex)
-            print(out_ascii)
-
-            #print("No match")
-
-
-
-def main(argv):
-    rx_hex = r'^(0x|x|\\x)([0-9a-f]{2}$)'
-    rx_int = r'[0-9]+'
-
-    if len(argv) != 2:
-        print("Only one arg accepted")
-        return
-
-    mode = argv[0]
-    if mode not in ('h', 'a', 'i'):
-        print("Unsupported mode")
-        return
-
-    value = argv[1]
-    
-    out_ascii = []
-    out_int = []
-    out_hex = []
-
-    if mode == 'h':
-        char_array = value.split()
-        for c in char_array:
-            match = re.match(rx_hex, c)
-            if match:
-                value = match.group(2)
-                value_int = ord(binascii.unhexlify(value))
-                out_int.append(value_int)
-                out_hex.append(hex(value_int))
-                out_ascii.append(chr(value_int))
-
-    if mode == 'a':
-        for c in value:
-            value_int = ord(c)
-            out_int.append(value_int)
-            out_hex.append(hex(value_int))
-            out_ascii.append(chr(value_int))
-
-    if mode == 'i':
-        int_array = value.split()
-        for s in int_array:
-            value_int = int(s)
-            out_int.append(value_int)
-            out_hex.append(hex(value_int))
-            out_ascii.append(chr(value_int))
-
-    print(out_int)
-    print(out_hex)
-    print(out_ascii)
-
+            print("Input: {} - BYTE-ARRAY".format(data_string))
+            hex_str = binascii.hexlify(data_string)
+            (h, a, i) = hex_to_pretty(hex_str)
+            print_hai(h, a, i)
+            return
 
 
 
 if __name__ == "__main__":
+    argc = len(sys.argv)
     #main(sys.argv[1:])
-    characterize_data(sys.argv[1])
+    if argc == 1:
+        # Read from stdin
+        characterize_data(sys.stdin.read())
+    else:
+        characterize_data(sys.argv[1])
